de9c064735bd0e18c19482b24b617860aedf766e
Lev Michael <lev.choubine@gmail.com>
2020-10-22 00:51:54 -0400

    [js] add javaScript cvhallenge solutions

--- starter_code/main.js
+++ starter_code/main.js
@@ -1,62 +1,121 @@
 // Question 1
-function maxOfTwoNumbers() {
+// Good Morning!! Hope your day is going well!!!
+function maxOfTwoNumbers(num1,num2) {
+   if(num1 > num2){
+       return num1;
+   }else{
+       return num2;
+   }
 
-}
+ 
+};
+
+console.log(maxOfTwoNumbers(11,5));
 
 // Question 2
-function maxOfThree() {
-
+function maxOfThree(num1,num2,num3) {
+    return Math.max(num1,num2,num3) ;
 }
+console.log(maxOfThree(-1,-8,-2));
 
 // Question 3
-function isCharacterAVowel() {
+const vowelArray =['A', 'E', 'I', 'O', 'U'];
+let yourCharacter ='K'; //Hard Code//
+// We haven't addressed input methods in class just yet  - so I will be hard -oding the function argument for now.
+function isCharacterAVowel(yourCharacter) {
+  if(vowelArray.includes(yourCharacter)){
+      console.log("Your Character is indeed a VOWEL!")
+      return true;
+  }else{
+      console.log('Sorry, Your chosen character seems to be a consonant...')
+      return false;
+  }
+};
 
-}
+console.log(isCharacterAVowel(yourCharacter));
 
 // Question 4
-function sumArray() {
- 
-}
+let b;
+const wantToAddUpSomeNumbers= [1,2,3,4];
+let resultValue = 0;
+function sumArray(array) {
+ for(b = 0; b < array.length; b++ ) {
 
+     resultValue = resultValue + array[b];
+ } 
+ return resultValue;
+
+};
+console.log(sumArray(wantToAddUpSomeNumbers));
 
 // Question 4
-function multiplyArray() {
 
+const wantToMultiplySomeNumbers= [1,2,3,4];
+let resultsValue = 1;
+
+let a
+function multiplyArray(array) {
+    for(a = 0; a < array.length; a++ ) {
+   
+        resultsValue = resultsValue * array[a];
+    } 
+    return resultsValue;
+};
+console.log(multiplyArray(wantToMultiplySomeNumbers));
+
+//Question 5
+// the function below will return "3"
+const numberOfArguments = function(functionToAnalyze){
+   return functionToAnalyze.length;
+    
 }
-
-
-// Question 5
-const numberOfArguments = function(){
-  
-}
-
+console.log(numberOfArguments(maxOfThree));
 
 
 // Question 6
-const reverseString = function (){
-  
-};
+const UserInput ="Happy Thursday!"
 
+const reverseString = function (line){
+    let breakDownTheString = line.split("");
+    let reverseArray = breakDownTheString.reverse();
+    let backwarsString = reverseArray.join(""); 
+    return backwarsString;   
+};
+console.log(reverseString(UserInput));
 
 // Question 7
-function findLongestWord () {
-  
+//expecting '10' as the longest word length
+
+const longWordArray = ['Bike', 'Passport', 'Cat', 'Apprentice'];
+let longestWord = 0;
+function findLongestWord (array) {
+    for(let i = 0; i < array.length; i++){
+        if(array[i].length > longestWord){ 
+            longestWord = array[i].length;
+        }
+        
 }
+return longestWord;
+};
+ console.log(findLongestWord(longWordArray));
 
 
 // Question 8
-function filterLongWords () {
-  
-}
+// Should not return 'Me'!
+const lastArray = ['Building', 'Me', 'infrastructure', 'Apprentice'];
+const setYourLimit = 5;
+let i = 6 ;//user imnut
+let resultArray = [ ];
+function filterLongWords (array,i) {
+    for(let y = 0; y < array.length; y++){
+      if(array[y].length > i){
+        resultArray.push(array[y]); 
+      }
 
+    } 
+    return resultArray;
+    
+};
+console.log(filterLongWords(lastArray, i));
 
-// Bonus 1
-function logBetween(lowNum, highNum) {
- 
-}
-
-// Bonus 2
-function charactersOccurencesCount() {
-  
-}
 
