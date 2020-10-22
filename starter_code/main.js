d680df69ae67b290893b52bd39e6eab4e76717a3
Edree Huss <edreeshuss@gmail.com>
2020-10-22 03:00:37 -0400

    [FunctionsJS] Fix functions in js file

--- starter_code/main.js
+++ starter_code/main.js
@@ -1,61 +1,109 @@
 // Question 1
-function maxOfTwoNumbers() {
 
+function maxOfTwoNumbers(a, b) {
+    if (a < b) { 
+        return b;
+    } else {
+        return a;
+    }
 }
+console.log("The max of the two numbers in the function is ", maxOfTwoNumbers(23,42));
+
 
 // Question 2
-function maxOfThree() {
-
-}
-
+function maxOfThree(a,b,c) {
+    if (a <= b && b <= c) {
+        return c ;
+     } else if (b <= a && c <= a) {
+         return a;
+     } else {
+         return b;
+     }
+     } 
+     console.log("The max of the three numbers in function is ", maxOfThree(4,67,48));
 // Question 3
-function isCharacterAVowel() {
 
+function isCharacterAVowel(letter) {
+    if (letter == 'A' || letter == 'E' || letter == 'I' || letter == 'I' || letter == 'O' || letter == 'U') {
+        return true;
+    } else { 
+        return false;
+    }
 }
+console.log(isCharacterAVowel('p'));
 
 // Question 4
 function sumArray() {
- 
+
+    const sumArray = [3, 9, 1, 4];
+    let sum = 0;
+    for (let i = 0; i < sumArray.length; i++) {
+        sum = sum + sumArray[i];
+    }
+    return sum;
 }
+
+console.log(sumArray()); 
 
 
 // Question 4
 function multiplyArray() {
 
+    const multArray= [10, 2, 1, 5];
+    let mul = 1;
+    for (let i = 0; i < multArray.length; i++) {
+        mul = mul * multArray[i];
+    }
+    return mul;
 }
+console.log(multiplyArray());
 
 
 // Question 5
-var numberOfArguments = function(){
-  
-}
-
-
+const numberOfArguments = function(){
+     return arguments.length;
+ }
+console.log(numberOfArguments("argument1", "argument2", "argument3", "argument4")) 
+console.log(numberOfArguments(38, true, false, "hello i am an argument", true, false))
 
 // Question 6
-var reverseString = function (){
-  
-};
-
+const reverseString = function (string1){
+    return string1.split('').reverse().join('')
+}
+console.log(reverseString('hello hi'))
+// shows: olleh ih
+console.log(reverseString('olleh ih'))
+// Shows:  hello hi
+console.log(reverseString('car'))
+// shows: rac
+console.log(reverseString('rac'))
+// Shows:  car
 
 // Question 7
-function findLongestWord () {
-  
+function findLongestWord(array) {
+    let word = '';
+    for (let i = 0; i <array.length; i++) {
+        if (word.length < array[i].length) {
+            word = array[i];
+        }
+    }
+    return word;
 }
+console.log(findLongestWord(['check','queen','checkmate', 'chessboard']));
+console.log(findLongestWord(['mercedes','hondo','toyota', 'audi']));
+console.log(findLongestWord(['salmon','red', 'pink']));
 
 
 // Question 8
-function filterLongWords () {
-  
+function filterLongWords(wordLength, array) {
+    let newArray = [];
+    for (let i = 0; i <array.length; i++) {
+        if (array[i].length > wordLength) {
+                newArray.push(array[i]);
+                }
+        }
+    return newArray;
 }
-
-
-// Bonus 1
-//??????
-
-
-// Bonus 2
-function charactersOccurencesCount() {
-  
-}
-
+console.log(filterLongWords(5,['check','queen','checkmate', 'chessboard']));
+console.log(filterLongWords(7,['mercedes','hondo','toyota', 'audi']));
+console.log(filterLongWords(5,['salmon','red', 'pink', 'black', 'purple']));
