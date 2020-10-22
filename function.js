74e581e71764e15b96b6b6d3a3a59b07f669879d
MusicMan <musicman@MacBook-Pro.fios-router.home>
2020-10-21 21:30:43 -0400

    [functions-deliverable] function assignment

---
+++ function.js
@@ -0,0 +1,90 @@
+
+  function maxOfTwoNumbers (x,y){
+      var number1 = x;
+      var number2 = y;
+      if (number1 > number2) {
+          return("x is greater!");
+      } else if (number2 > number1) {
+          return("Y is greater!")
+      } else {
+          return ("They are Equal!");
+      }
+  }
+maxOfTwoNumbers(3,6);
+// 
+
+function maxOfThree(a, b, c,) {
+
+    for (let business of maxOfThree) {
+
+    
+    var number1 = a;
+    var number2 = b;
+    var number3 = c;
+    if (number 1 > Number 2 && number 1 > Number 3) {
+        return("Number 1 is is Number 1!");
+        if else (number 2 > Number 1 && Number 2 > Number 3) {
+            return("Number 2 is  2 good!");
+            else if(business !> number1 || business === number1, number2, number3) {
+                return:("They are Equals!");
+            }
+    }
+
+function vowelCheck(y){
+
+    let pet = crocodile;
+
+        if( y === "A" || y === "E" || y === "I" || y === "O" || y === "U" ) {
+            result = true;
+            return result;
+        }
+        else{
+            result = false;
+            return result; 
+        }
+   vowelCheck();
+    }
+
+     function addArray(num){
+        return num.reduce(function(a,b,c){
+          return a + b + c
+        }
+    comsole.log("addArray");
+    multiplyNum = function(arg){
+        return arg.reduce(function(a,b,c){
+          return a * b * c
+        }
+      }
+    }
+// wow I was having so much trouble with this one. 
+// I didn't even know where to start (besides looking it up)
+function (a, b) {
+return arguments.length;
+}
+
+function reverseString(str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+reverseString('hello');
+
+
+function takesLongestWord (list) {
+    let list = ['swim', 'sharks', 'turtles'];
+return list.Max();
+}
+
+// Same.. This was also Darn tough.
+var filterLongWords = function(array, int){
+    var length = array.length;
+    var longestWords = [];
+    for (i = 0; i < length; i++) {
+      if (array[i].length > int) {
+        longestWords[longestWords.length] = array[i];
+      }
+    }
+    return longestWords;
+  }
