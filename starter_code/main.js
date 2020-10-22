3c942775d8b36c8cf81693eb1ca72a65aac3f37e
Will Locklier <will.locklier@gmail.com>
2020-10-21 23:39:22 -0400

    [updated js file] Add updated JS file

--- starter_code/main.js
+++ starter_code/main.js
@@ -1,54 +1,102 @@
 // Question 1
-function maxOfTwoNumbers() {
+function maxOfTwoNumbers(firstNum, secondNum) {
+    if (firstNum > secondNum) {
+        console.log(firstNum + " is larger than " + secondNum);
+    } else {
+        console.log(firstNum + " is less than " + secondNum);
+    }
 
 }
+maxOfTwoNumbers(100, 200);
 
 // Question 2
-function maxOfThree() {
+function maxOfThree(firstNum, secondNum, thirdNum) {
+    console.log(Math.max(firstNum, secondNum, thirdNum));
 
 }
+maxOfThree(7, 2, 3);
 
 // Question 3
-function isCharacterAVowel() {
+function isCharacterAVowel(isLetter) {
+    if (isLetter === 'a' || 'e' || 'i' || 'o' || 'u') {
+        console.log("The Character is a vowel");
+    } else {
+        console.log("This character is a consonant");
+    }
 
 }
+isCharacterAVowel("b")
+
 
 // Question 4
-function sumArray() {
+function sumArray(numbers) {
+    var total = 0;
+    for (var i = 0; i < numbers.length; i++) {
+        total +=numbers[i];
+    }
+    return total;
  
 }
+console.log(sumArray([1,2,3,4]));   
 
 
 // Question 4
-function multiplyArray() {
+function multiplyArray(numbers) {
+    var total = 1;
+    for (var i = 0; i < numbers.length; i++) {
+        total = (total * numbers[i]);
+    }
+    return total;
 
 }
-
+console.log(multiplyArray([1,2,3,4]));
 
 // Question 5
-var numberOfArguments = function(){
+// ... allows unlimited arguements
+var numberOfArguments = function(...args){
+    console.log(args.length);
+
+
   
 }
-
-
+numberOfArguments(3, 4, 6)
 
 // Question 6
-var reverseString = function (){
-  
-};
 
+// .split('') - splits a string into an array based on a seperator
+// .reverse() - reverses an array
+// .join - rejoins the letters
+var reverseString = function (string){
+    console.log(string.split('').reverse(string).join(''));
 
-// Question 7
-function findLongestWord () {
   
 }
-
+reverseString('abcde')
+// Question 7
+function findLongestWord (array) {
+    var longestWord = 0
+    for (var i = 0; i < array.length; i++){
+        if (array[i].length > longestWord){
+            longestWord = array[i].length;
+        }
+    } return longestWord;
+  
+}
+console.log(findLongestWord(['one', 'five', 'twenty']))
 
 // Question 8
-function filterLongWords () {
+function filterLongWords (array, num) {
+    var length = array.length;
+    var longestWords = [];
+    for ( i = 0; i < length; i++) {
+        if (array[i].length > num) {
+            longestWords[longestWords.length] = array[i];
+        }
+    }
+    return longestWords;
   
 }
-
+console.log(filterLongWords(["Michael", "John"], 3))
 
 // Bonus 1
 //??????
