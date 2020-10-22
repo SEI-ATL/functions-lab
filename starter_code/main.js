f09f1b09211cd44be0d0bbecefbb2421e8546d9b
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2020-10-22 07:46:22 -0500

    [HW] Add homework to Github

--- starter_code/main.js
+++ starter_code/main.js
@@ -1,42 +1,67 @@
 // Question 1
-function maxOfTwoNumbers() {
-
-}
+function maxOfTwoNumbers(numberOne, numberTwo) {
+        if (numberOne > numberTwo) {
+           console.log(numberOne + " is larger than " + numberTwo);
+        } else {
+            console.log(numberOne + " is less than " + numberTwo);
+        }
+    }
+    maxOfTwoNumbers(5,7);
 
 // Question 2
-function maxOfThree() {
-
+function maxOfThree (numberOne, numberTwo, numberThree) {     
+    return Math.max (numberOne, numberTwo, numberThree); 
 }
+  
+console.log(maxOfThree(3,12,27));
 
 // Question 3
-function isCharacterAVowel() {
-
+function isCharacterAVowel(letter) {
+    if (letter === 'a','e','i','o','u') {
+        return "The letter is a vowel";
+    } else {
+        return "The letter is not a vowel";
+    }
 }
+console.log(isCharacterAVowel('a'));
 
 // Question 4
-function sumArray() {
- 
+function sumArray(number) {
+    var sum = 0;
+    for (var i = 0; i < number.length; i++) {
+            sum += number[i];
+    }
+    return sum;
 }
-
+console.log(sumArray([1,2,3,4]))
 
 // Question 4
-function multiplyArray() {
-
+function multiplyArray(number) {
+    var sum = 1;
+    for (var i = 0; i < number.length; i++) {
+           sum = (sum * number[i]);
+    }
+    return sum;
 }
-
+console.log(multiplyArray([1,2,3,4]))
 
 // Question 5
 var numberOfArguments = function(){
-  
+    var arguments = "";
+    for (var i = 0; i < arguments.length; i++) 
+    return arguments;
 }
 
 
 
 // Question 6
-var reverseString = function (){
-  
-};
-
+var reverseString = function (string){
+    var text = "";
+    for (var i = string.length - 1; i >= 0; i--) {
+        text += string[i];
+    } return text;
+    }
+console.log(reverseString("jag testar"));
 
 // Question 7
 function findLongestWord () {
