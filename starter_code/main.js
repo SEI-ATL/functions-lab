3f3bd7583132723df329eb7ee0d0fa67093f307a
Nicole Hamilton <Nicole@Warps-MacBook-Air.local>
2020-10-21 19:49:30 -0700

    [js] add answers for questions 1 - 7. 8 was too difficult.

--- starter_code/main.js
+++ starter_code/main.js
@@ -1,47 +1,83 @@
 // Question 1
 function maxOfTwoNumbers() {
-
+    if (numOne > numTwo) {
+        console.log(numOne +' is greater than ' + numTwo);
+    } else {
+        console.log(numTwo + ' is the higher number');
+    }
 }
+let numOne=20
+let numTwo=30
+maxOfTwoNumbers();
 
 // Question 2
-function maxOfThree() {
-
+function maxOfThree(array) {
+   return Math.max(array);
 }
+let array=[2,5,1];
+console.log(maxofThree(array));
 
 // Question 3
-function isCharacterAVowel() {
-
+function isCharacterAVowel(val) {
+    val = val.toUpperCase()
+    let result = false
+        if( val === "A" || val === "E" || val === "I" || val === "O" || val === "U" ) {
+            result = true;
+        }
+        return result 
 }
+console.log(isCharacterAVowel("z"));
 
 // Question 4
-function sumArray() {
- 
+function sumArray(numbers) {
+ let sum=0;
+ for(const value of numbers) {
+     sum += value;
+ }
+ return sum;
 }
+sum([1,2,3,4,5]);
 
 
 // Question 4
-function multiplyArray() {
-
-}
+function multiplyArray(numbers) {
+    let num=1;
+      for(const value of numbers) {
+      num *= value;
+  }
+      return num;
+  }
+  console.log(multiplyArray([2,3,4]));
 
 
 // Question 5
-const numberOfArguments = function(){
-  
+var numberOfArguments = function(){
+    console.log(arguments)
+    return arguments.length
 }
+numberOfArguments(10, 20);
 
 
 
 // Question 6
-const reverseString = function (){
-  
-};
+const reverseString = function reverseString(rev) {
+    return rev.split("").reverse().join("");
+}
+console.log(reverseString("hello"));
 
 
 // Question 7
-function findLongestWord () {
-  
-}
+function findLongestWord(x) {
+    let split = x.split(' ');
+    let longestWord = 0;
+    for(let i = 0; i < split.length; i++){
+      if(split[i].length > longestWord){
+      longestWord = split[i].length;
+       }
+    }
+    return longestWord;
+  }
+  console.log(findLongestWord("the mitochondria is the powerhouse of the cell"));
 
 
 // Question 8
