9bcf42ed63aec32aa758aaee34b3f73cfe9f2773
swolepenguin <ashtonm223@gmail.com>
2020-10-21 22:07:39 -0400

    this is my revised javascript

--- starter_code/main.js
+++ starter_code/main.js
@@ -1,35 +1,63 @@
 // Question 1
-function maxOfTwoNumbers() {
+function maxOfTwoNumbers(num1, num2) {
+    let max = Math.max(num1, num2) 
+    return max;
 
 }
-
+console.log( maxOfTwoNumbers(1,2))
 // Question 2
-function maxOfThree() {
-
+function maxOfThree(num3, num4, num5) {
+    let max = Math.max(num3, num4, num5)
+    return max;
 }
+console.log(maxOfThree(3, 4, 5))
 
 // Question 3
-function isCharacterAVowel() {
+function isCharacterAVowel(letter1) {
+    let vowel = ['A', 'E', 'I', 'O', 'U']
+    for (let i = 0; i<5; i++){
+        if (vowel[i] == letter1.toUpperCase()) {
+            console.log('Character is a vowel')
+            return true        
+        }
+           
+    } 
+    return false
+    }
+    isCharacterAVowel('a')
+
+// Question 4
+function sumArray(a) {
+    var total=0 
+    for ( var i in a) {
+        total += a[i];
+    }    
+
+console.log(totalNumbers)
 
 }
 
-// Question 4
-function sumArray() {
- 
-}
 
 
 // Question 4
-function multiplyArray() {
+function multiplyArray(a) {
+    var numbers = [4,5,6]; 
+    var totalNumbers= multiplyArray(numbers);
+    var total=0 
+    for ( var i in a) {
+        total *= a[i];
+    }    
+console.log(totalNumbers)
 
 }
 
 
 // Question 5
 var numberOfArguments = function(){
-  
+    console.log(arguments)
+    return arguments.length
 }
-
+numberOfArguments(1,2)
 
 
 // Question 6
@@ -39,10 +67,16 @@ var reverseString = function (){
 
 
 // Question 7
-function findLongestWord () {
-  
-}
+function findLongestWord (words) {
+  let findLongestWord = ""
+  for (i = 0; i < words.length; i++){
+      if(words[i].length > findLongestWord.length){
 
+      }
+  }
+  return longestWord
+}
+const words = ['hello', 'goodbye', 'morning']
 
 // Question 8
 function filterLongWords () {
