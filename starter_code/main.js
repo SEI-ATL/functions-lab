7de61bf0cc9bca902c7a5fcab58d4aa23d9ff575
Luke Iverson <lukeiverson@Lukes-MacBook-Pro.local>
2020-10-21 19:22:24 -0500

    Luke Iverson functions

--- starter_code/main.js
+++ starter_code/main.js
@@ -1,53 +1,107 @@
 // Question 1
-function maxOfTwoNumbers() {
-
+function max(firstNumber, secondNumber){
+  
+    if (firstNumber > secondNumber) {
+       console.log(firstNumber + " is larger than " + secondNumber);
+    } else {
+        console.log(firstNumber + " is less than " + secondNumber);
+    }
+    return;
+  
 }
+max(79,118);
+
+
 
 // Question 2
-function maxOfThree() {
-
+function maxOfThree(num1, num2, num3) {     
+    return Math.max (num1, num2, num3); 
 }
+  
+console.log (maxOfThree (11,19,48));
+
+
 
 // Question 3
-function isCharacterAVowel() {
-
+function vowelChecker (char) {
+  
+    if (char === 'a', 'e', 'i', 'o', 'u') {
+        return "The character is a Vowel";
+    }
+    else {
+    return "The character is a Consonant";
+    }
 }
+  
+console.log (vowelChecker ("e"));
 
 // Question 4
-function sumArray() {
- 
+function sum(numbers) {
+	var total = 0;
+	for (var i = 0; i < numbers.length; i++) {
+		total += numbers[i];
+	}
+	return total;
 }
+
+console.log(sum([1,2,3,4]));
 
 
 // Question 4
-function multiplyArray() {
-
+function multiply(numbers) {
+	var total = 1;
+	for (var i = 0; i < numbers.length; i++) {
+		total = (total * numbers[i]);
+	}
+	return total;
 }
+
+console.log(multiply([1,2,3,4]));
 
 
 // Question 5
 var numberOfArguments = function(){
-  
-}
+console.log(arguments)  
+return arguments.length
 
+}
+console.log(numberOfArguments(1,2));
 
 
 // Question 6
-var reverseString = function (){
-  
-};
-
+function reverseString(hello) {
+    hello= hello.split('')
+    console.log(hello)
+    hello= hello.reverse()
+    console.log(hello)
+    return hello.join('')
+}
+console.log(reverseString('hello'))
 
 // Question 7
-function findLongestWord () {
-  
+function findLongestWord (words) {
+  let longestWord = '';
+  for (i = 0; i < words.length; i++){
+      if (words[i].length > longestWord.length){
+          longestWord = words[i];
+      }
+  } 
+  return longestWord
 }
-
+const words = ['hello', 'goodbye', 'morning']
+console.log(findLongestWord(words))
 
 // Question 8
-function filterLongWords () {
-  
+function filterLongWords(wordLength, array) {
+    let newArray = [];
+    for (let i = 0; i <array.length; i++) {
+        if (array[i].length > wordLength) {
+                newArray.push(array[i]);
+                }
+        }
+    return newArray;
 }
+console.log(filterLongWords(6,['hello','goodbye','dictionary'])); 
 
 
 // Bonus 1
