dfc2609b482c05be3dd4771b9a15ac69261bb658
Nitish Dayal <contact@nitishdayal.me>
2020-10-26 07:08:21 -0700

    feat: Base assignment complete

--- starter_code/main.js
+++ starter_code/main.js
@@ -1,62 +1,77 @@
 // Question 1
-function maxOfTwoNumbers() {
+function maxOfTwoNumbers(num1, num2) {
+  let returnVal = Math.max(num1, num2)
 
+  return returnVal
 }
 
 // Question 2
 function maxOfThree() {
-
+  return Math.max(...arguments)
 }
 
 // Question 3
-function isCharacterAVowel() {
+function isCharacterAVowel(char) {
+  if (char.length > 1) return `Provided argument more than 1 characters`
 
+  return ['a', 'e', 'i', 'o', 'u'].indexOf(char.toLowerCase()) !== -1
 }
 
 // Question 4
-function sumArray() {
- 
+function sumArray(arrOfInts) {
+  return arrOfInts.reduce((a, b) => a + b)
 }
 
 
 // Question 4
-function multiplyArray() {
-
+function multiplyArray(arrOfInts) {
+  return arrOfInts.reduce((a, b) => a * b)
 }
 
 
 // Question 5
-const numberOfArguments = function(){
-  
+const numberOfArguments = function () {
+  return arguments.length
 }
 
 
 
 // Question 6
-const reverseString = function (){
-  
+const reverseString = function (str) {
+  return str.split('').reverse().join('');
 };
 
 
 // Question 7
-function findLongestWord () {
-  
+function findLongestWord(arrOfWords) {
+  let longestWordLength = 0
+
+  arrOfWords.forEach(word => {
+    if (word.length > longestWordLength) longestWordLength = word.length
+  })
+
+  return longestWordLength
 }
 
 
 // Question 8
-function filterLongWords () {
-  
+function filterLongWords(arrOfWords, i) {
+  let filteredArray = []
+
+  arrOfWords.forEach(word => {
+    if (word.length > i) filteredArray.push(word)
+  })
+
+  return filteredArray
 }
 
 
 // Bonus 1
 function logBetween(lowNum, highNum) {
- 
+
 }
 
 // Bonus 2
 function charactersOccurencesCount() {
-  
-}
 
+}
